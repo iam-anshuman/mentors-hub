d175d26c76a00a7687a2afa6fd988a39b929fafe
Anshuman Tiwari <118616967+iam-anshuman@users.noreply.github.com>
2024-06-08 17:22:31 +0530

    Navbar configured for Mobile Device

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -30,7 +30,11 @@ export default function Navbar() {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </div>
         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <Link href={"/mentors"}><li>Mentors</li></Link>
+          {user?.role === "mentor" ? 
+            <Link href={"/mentors-panel"} className='py-2'><li>Mentors Panel</li></Link>
+            :
+            <Link href={"/mentors"} className='py-2'><li>Mentors</li></Link>
+          }
           <li>
             <a>About Us</a>
             <ul className="p-2">
