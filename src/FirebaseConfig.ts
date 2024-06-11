8f79f697a6fe17137e99a77bf5ad1f990173814d
Anshuman Tiwari <118616967+iam-anshuman@users.noreply.github.com>
2024-06-11 08:07:31 +0530

    Firebase config changed

--- src/FirebaseConfig.ts
+++ src/FirebaseConfig.ts
@@ -7,13 +7,12 @@ dotenv.config();
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA3pPxssgM_eQaXqlHXt1Yw_5ZmtRPCrGk",
-  authDomain: "mentorhub-639b2.firebaseapp.com",
-  projectId: "mentorhub-639b2",
-  storageBucket: "mentorhub-639b2.appspot.com",
-  messagingSenderId: "591667234683",
-  appId: "1:591667234683:web:613aafb11786183a55dfde",
-  measurementId: "G-7W42TT1MPW"
+  apiKey: "AIzaSyDp5vTcEIG5qS4IgwKaCw0UMF89MuiXRKc",
+  authDomain: "mentors-connect.firebaseapp.com",
+  projectId: "mentors-connect",
+  storageBucket: "mentors-connect.appspot.com",
+  messagingSenderId: "956262261116",
+  appId: "1:956262261116:web:390c6148bd905ab5b2c4f9"
 };
 
 
